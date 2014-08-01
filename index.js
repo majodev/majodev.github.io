9f6a5db1ba4d5c69e9f2ebc44ccaacea04ad7a50
majodev <contact@majodev.com>
2014-08-01 03:36:35 +0200

    custom plugin housekeeping

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ var branch = require('metalsmith-branch');
 
 // custom metalsmith scripts (aka plugins)
 var deletehiddenfiles = require("./scripts/metalsmith-deletehiddenfiles");
-var printfilesmeta = require("./scripts/metalsmith-printfilesmeta");
-var contenthandlebars = require("./scripts/metalsmith-contenthandlebars");
-var metasetpermalinks = require("./scripts/metalsmith-metasetpermalinks");
-var metaapplypermalinks = require("./scripts/metalsmith-metaapplypermalinks");
+var debugsmith = require("./scripts/metalsmith-debugsmith");
+var hbs = require("./scripts/metalsmith-hbs");
+var permapathpre = require("./scripts/metalsmith-permapathpre");
+var permapathpost = require("./scripts/metalsmith-permapathpost");
 var highlightjs = require("./scripts/metalsmith-highlightjs");
-var formatdate = require("./scripts/metalsmith-formatdate");
+var metaformat = require("./scripts/metalsmith-metaformat");
 var permatags = require("./scripts/metalsmith-permatags");
 
 // custom node scripts
@@ -35,9 +35,9 @@ Metalsmith(__dirname)
   .source("./src")
   .destination("./build")
   .use(deletehiddenfiles())
-  .use(metasetpermalinks())
+  .use(permapathpre())
   .use(permatags())
-  .use(formatdate())
+  .use(metaformat())
   .use(branch()
     .pattern("!404.*")
     .use(collections({
@@ -57,7 +57,7 @@ Metalsmith(__dirname)
       "template": "note.hbs"
     }
   }]))
-  .use(contenthandlebars())
+  .use(hbs())
   .use(markdown())
   .use(highlightjs({
     tabReplace: '  '
@@ -66,8 +66,8 @@ Metalsmith(__dirname)
     engine: "handlebars",
     directory: "templates"
   }))
-  .use(metaapplypermalinks())
-// .use(printfilesmeta({
+  .use(permapathpost())
+// .use(debugsmith({
 //   printMetaKeys: false
 // }))
 .build(function(error) {
