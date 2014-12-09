dca399416f5230b75351543ac64af24c0eaf9dc4
majodev <contact@majodev.com>
2014-12-09 16:06:59 +0100

    Generated from branch source commit ab657b0

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2014-12-06 16:22:16 */
+/*! majodev.github.io - v1.0.0 - build 2014-12-09 16:05:55 */
 !function(){var a=$("html, body");$(".smoothScroll").click(function(){var b=$.attr(this,"href");return"undefined"!=typeof b?(a.animate({scrollTop:$(b).offset().top},500,function(){window.location.hash=b}),!1):void 0})}();
