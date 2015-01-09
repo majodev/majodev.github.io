cfec5f51bed5b3c287ef3925c42cc9556705b028
majodev <contact@majodev.com>
2015-01-09 18:03:02 +0100

    Generated from branch source commit b4b2635

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2015-01-09 13:01:44 */
+/*! majodev.github.io - v1.0.0 - build 2015-01-09 18:02:49 */
 !function(){var a=$("html, body");$(".smoothScroll").click(function(){var b=$.attr(this,"href");return"undefined"!=typeof b?(a.animate({scrollTop:$(b).offset().top},500,function(){window.location.hash=b}),!1):void 0})}();
--- media/media.js
+++ media/media.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2015-01-09 13:01:44 */
+/*! majodev.github.io - v1.0.0 - build 2015-01-09 18:02:49 */
 var container=document.querySelector("#masonryContainer"),msnry=new Masonry(container,{itemSelector:".masonryItem"}),imgLoad=imagesLoaded(container);imgLoad.on("progress",function(){msnry.layout()}),imgLoad.on("done",function(){msnry.layout()});
