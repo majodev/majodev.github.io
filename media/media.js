ab5e7ad9a25487306c557990bfaf07b7405b9d8c
majodev <contact@majodev.com>
2015-01-10 14:28:41 +0100

    Generated from branch source commit 6918742

--- media/media.js
+++ media/media.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2015-01-09 22:17:46 */
+/*! majodev.github.io - v1.0.0 - build 2015-01-10 14:28:28 */
 var container=document.querySelector("#masonryContainer"),msnry=new Masonry(container,{itemSelector:".masonryItem"}),imgLoad=imagesLoaded(container);imgLoad.on("progress",function(){msnry.layout()}),imgLoad.on("done",function(){msnry.layout()});
