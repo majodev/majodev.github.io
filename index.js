2f7f14e248a229e45712201ece24cd7453f692e1
majodev <contact@majodev.com>
2015-01-17 17:22:54 +0100

    Generated from branch source commit 3597200

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2015-01-17 17:01:58 */
+/*! majodev.github.io - v1.0.0 - build 2015-01-17 17:22:43 */
 !function(){var a=$("html, body");$(".smoothScroll").click(function(){var b=$.attr(this,"href");return"undefined"!=typeof b?(a.animate({scrollTop:$(b).offset().top},500,function(){window.location.hash=b}),!1):void 0})}();
