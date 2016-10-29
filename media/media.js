d6e584c67da8966333bc7af65b267f5acc4c83ea
Mario Ranftl <mario.ranftl@allaboutapps.at>
2016-10-29 19:41:21 +0200

    Generated from branch source commit 0a2f59b

--- media/media.js
+++ media/media.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2015-08-10 20:36:36 */
+/*! majodev.github.io - v1.0.0 - build 2016-10-29 19:41:07 */
 var container=document.querySelector("#masonryContainer"),msnry=new Masonry(container,{itemSelector:".masonryItem"}),imgLoad=imagesLoaded(container);imgLoad.on("progress",function(){msnry.layout()}),imgLoad.on("done",function(){msnry.layout()});
