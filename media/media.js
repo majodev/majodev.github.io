aa06056d096e9e6380c8e19e4d3c20af309cabd6
Mario Ranftl <code@mranftl.com>
2017-05-22 21:08:25 +0200

    Generated from branch source commit dcf6fda

--- media/media.js
+++ media/media.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2017-05-15 22:20:15 */
+/*! majodev.github.io - v1.0.0 - build 2017-05-22 21:08:18 */
 var container=document.querySelector("#masonryContainer"),msnry=new Masonry(container,{itemSelector:".masonryItem"}),imgLoad=imagesLoaded(container);imgLoad.on("progress",function(){msnry.layout()}),imgLoad.on("done",function(){msnry.layout()});
