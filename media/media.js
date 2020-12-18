fc607f576f0b454ffc62f2d52305f4d70da795bd
Mario Ranftl <code@mranftl.com>
2020-12-18 15:10:45 +0100

    Generated from branch source commit bda993c

--- media/media.js
+++ media/media.js
@@ -1,2 +1,2 @@
-/*! majodev.github.io - v1.0.0 - build 2020-05-18 20:41:58 */
+/*! majodev.github.io - v1.0.0 - build 2020-12-18 15:10:06 */
 var container=document.querySelector("#masonryContainer"),msnry=new Masonry(container,{itemSelector:".masonryItem"}),imgLoad=imagesLoaded(container);imgLoad.on("progress",function(){msnry.layout()}),imgLoad.on("done",function(){msnry.layout()});
